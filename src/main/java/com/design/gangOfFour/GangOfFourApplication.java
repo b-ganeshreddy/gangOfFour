32cf15e28c6ac976101a6089a522c5bf8c22943d
GaneshReddy <ganeshreddy.b@icloud.com>
2021-06-29 20:49:25 +0530

    Basic spring files

---
+++ src/main/java/com/design/gangOfFour/GangOfFourApplication.java
@@ -0,0 +1,13 @@
+package com.design.gangOfFour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangOfFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangOfFourApplication.class, args);
+	}
+
+}
